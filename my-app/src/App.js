4cbaad753bd260cc56e8d636dc5fa8e28407cdaa
Fznr <fauzan197@gmail.com>
2020-06-07 21:42:37 +0700

    initial  commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hai from apps</h1>
+    </>
+  );
+}
+
+export default App;
